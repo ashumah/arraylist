f5b7326efcb09b70c6f9964547e825ce27f1df95
Your Name <you@example.com>
2022-02-26 21:38:24 +0530

    commited

---
+++ day14/src/main/java/day14/arraylist.java
@@ -0,0 +1,22 @@
+package day14;
+
+
+
+import java.util.ArrayList;
+
+public class arraylist {
+
+
+  public static void main(String[] args){
+
+    // create ArrayList
+    ArrayList<String> languages = new ArrayList<String>();
+
+    // Add elements to ArrayList
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("Swift");
+    System.out.println("ArrayList: " + languages);
+  }
+}
+
